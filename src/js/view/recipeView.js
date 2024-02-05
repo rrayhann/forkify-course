ae9f3af26cb55c3348256c6ca6622aa6002a44d5
Rayhan Armand <80816570+rrayhann@users.noreply.github.com>
2024-02-05 11:45:11 +0700

    fix: search button

--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -3,6 +3,7 @@ import View from './View.js';
 
 import icons from 'url:../../img/icons.svg'; //parcel 2
 import { Fraction } from 'fractional';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -126,7 +127,7 @@ class RecipeView extends View {
             <use href="${icons}#icon-check"></use>
           </svg>
           <div class="recipe__quantity">${
-            ing.quantity ? new Fraction(ing.quantity).toString() : ``
+            ing.quantity ? new fracty(ing.quantity).toString() : ``
           }</div>
           <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
